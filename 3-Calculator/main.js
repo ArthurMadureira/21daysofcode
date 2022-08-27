f90c4fa1c4bd98c4a54e52a25ab0d7885de1ca75
Arthur Madureira <arthurmadureira10@gmail.com>
2022-08-26 22:11:30 -0300

    fix some errors on the code of calculator challenge file

--- 3-Calculator/main.js
+++ 3-Calculator/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const result = document.querySelector(".value")
 const btns = document.querySelectorAll(".btn")
 const equalBtn = document.querySelector(".equal")
@@ -24,30 +23,5 @@ backspace.addEventListener("click", () => {
   result.innerHTML = result.textContent.substring(0, result.textContent.length - 1)
 })
 
-=======
-const result = document.querySelector(".value")
-const btns = document.querySelectorAll(".btn")
-const equalBtn = document.querySelector(".equal")
-const clearAll = document.querySelector(".clear-all")
-const backspace = document.querySelector(".del")
-
-btns.forEach(btn => {
-  btn.addEventListener("click", (e) => {
-    result.textContent += e.target.textContent
-  })
-})
-
-clearAll.addEventListener("click", () => {
-  result.textContent = ""
-})
 
 
-equalBtn.addEventListener("click", () => {
-  result.textContent = eval(result.textContent)
-})
-
-backspace.addEventListener("click", () => {
-  result.innerHTML = result.textContent.substring(0, result.textContent.length - 1)
-})
-
->>>>>>> 8ae228d4c8bf432bc6e83b83d6eba9951a50014b
