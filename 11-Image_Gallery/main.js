165c0521d71d487ff446a385e8432e1edf7de0e5
Arthur Madureira <arthurmadureira10@gmail.com>
2022-08-28 00:21:44 -0300

    update Image Gallery challenge

--- 11-Image_Gallery/main.js
+++ 11-Image_Gallery/main.js
@@ -1,8 +1,9 @@
 const images = document.querySelectorAll("img")
 const modalOverlay = document.querySelector(".modal-overlay")
 const imgModal = document.querySelector(".image-modal")
-
 const close = document.querySelector(".material-symbols-outlined")
+
+const content = document.querySelector(".content")
 
 images.forEach(img => {
   img.addEventListener("click", (e) => {
@@ -16,3 +17,55 @@ images.forEach(img => {
 close.addEventListener("click", () => {
   modalOverlay.classList.remove("active")
 })
+
+
+function addImage(url) {
+  const imageDiv = document.createElement("div")
+  imageDiv.classList.add("image-div")
+
+  const image = document.createElement("img")
+  image.src = url
+
+  content.appendChild(imageDiv)
+  imageDiv.appendChild(image)
+
+
+
+}
+
+const imgs = JSON.parse(localStorage.getItem("image")) || []
+
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+
+
+  const imgURL = document.querySelector("input")
+
+  if (imgURL.value != "") {
+
+    addImage(imgURL.value)
+    imgs.push(imgURL.value)
+
+    localStorage.setItem("image", JSON.stringify(imgs))
+
+  } else {
+    imgURL.style.border = "2px solid red"
+  }
+
+  imgURL.value = ""
+
+
+
+})
+
+
+
+
+imgs.forEach(i => {
+  addImage(i)
+})
+
